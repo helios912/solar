05972d92b86848ed58c8568dd429feae7f7fd077
Mikola <das19901990a@gmail.com>
2024-05-07 22:13:58 +0300

    adaptiv

--- js/main.js
+++ js/main.js
@@ -15,4 +15,21 @@ $(function () {
     prevArrow:
       '<button class="slick-arrow slick-prev"><img src="../images/arrow-next.svg" alt=""></button>',
   });
+  $(".client__foto").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    nextArrow:
+      '<button class="slick-arrow slick-next"><img src="../images/arrow-prev.svg" alt=""></button>',
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><img src="../images/arrow-next.svg" alt=""></button>',
+    asNavFor: ".client__text-carusel",
+  });
+  $(".client__text-carusel").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: false,
+    asNavFor: ".client__foto",
+  });
 });
